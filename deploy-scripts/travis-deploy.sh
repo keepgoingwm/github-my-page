6ad891b73fc486511d06afcdc923c10ae514481b
keepgoingwm <keepgoingwm@gmail.com>
2019-08-05 20:16:19 +0800

    部署方式修复

--- deploy-scripts/travis-deploy.sh
+++ deploy-scripts/travis-deploy.sh
@@ -1,26 +1,31 @@
 #!/bin/bash
+init_deploy_dir() {
+  cp public/ .deploy_git/ -rf
+}
+
 setup_git() {
+  git init
   git config --global user.email "keepgoingwm@gmail.com"
   git config --global user.name "keepgoingwm"
 }
 
-# commit_website_files() {
-#   if [ $TRAVIS_EVENT_TYPE != "pull_request" ]; then
-#     if [ $TRAVIS_BRANCH == "master" ]; then
-#       echo "Committing to master branch..."
-#       git checkout master
-#       git add *
-#       if [ $TRAVIS_EVENT_TYPE == "cron" ]; then
-#         git commit --message "Travis build: $TRAVIS_BUILD_NUMBER [cron]"
-#       elif [ $TRAVIS_EVENT_TYPE == "api" ]; then
-#         git commit --message "Travis build: $TRAVIS_BUILD_NUMBER [custom]"
-#       else
-#         git reset coverage/*
-#         git commit --message "Travis build: $TRAVIS_BUILD_NUMBER"
-#       fi
-#     fi
-#   fi
-# }
+commit_website_files() {
+  if [ $TRAVIS_EVENT_TYPE != "pull_request" ]; then
+    if [ $TRAVIS_BRANCH == "master" ]; then
+      echo "Committing to master branch..."
+      git checkout master
+      git add *
+      if [ $TRAVIS_EVENT_TYPE == "cron" ]; then
+        git commit --message "Travis build: $TRAVIS_BUILD_NUMBER [cron]"
+      elif [ $TRAVIS_EVENT_TYPE == "api" ]; then
+        git commit --message "Travis build: $TRAVIS_BUILD_NUMBER [custom]"
+      else
+        git reset coverage/*
+        git commit --message "Travis build: $TRAVIS_BUILD_NUMBER"
+      fi
+    fi
+  fi
+}
 
 upload_files() {
   if [ $TRAVIS_EVENT_TYPE != "pull_request" ]; then
@@ -31,6 +36,8 @@ upload_files() {
   fi
 }
 
+init_deploy_dir
 setup_git
-# commit_website_files
+cd .deploy_git
+commit_website_files
 upload_files
